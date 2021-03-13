47d769186dc7f7d15dc5069710d0c14c407d7f86
Andrea Cerfoglia <70335408+TheBigBoschi@users.noreply.github.com>
2021-03-13 12:36:40 +0100

    Update 1.0.ino

--- firmware FINAL/1.0.ino
+++ firmware FINAL/1.0.ino
@@ -10,6 +10,7 @@ byte getButton3();
 
 FaBoRTC_PCF2129 faboRTC;
 
+//pin definitions
 #define LATCH PIN_PA6
 #define PWM PIN_PA5
 #define CLOCK PIN_PA4
@@ -46,6 +47,7 @@ void buttonsCheck();
 
 void setup()
 {
+  //initialise the I2C pins on PA1 and PA2 
   Wire.pins(PIN_PA1, PIN_PA2);
 
   pinMode(LATCH, OUTPUT);
@@ -62,18 +64,23 @@ void setup()
   faboRTC.searchDevice();
   faboRTC.configure();
 
-  //inizializza per la prima volta l'orario di burnIn
+  //initializes for the first time the BurnIn function
   randomSeed(pwm);                                      
   randHour = (random(8,12) + faboRTC.getHours())%24;
   randMin = 30;
 
-  restoreSettings();
+  //restore from the eeprom the saved settings
+  restoreSettings();   
+  //it checks the buttons status
   buttonsCheck();
+  //is used as a workaround. the first time i call this function it return as the button is pressed. form the second time it work normally
   getButton3();
 
 
-  if(faboRTC.getSeconds() > 60 )    //se getSecond > 60 è perche l'rtc ha perso l'orario e va reimpostato
+  if(faboRTC.getSeconds() > 60 )    //if seconds >= 60 the RTC has lost power and has to be set again
     initializeClock();
+  
+  //this two elses are used to detect if a daylight saving time chiange has occurred while the clock was powered off
   else if(faboRTC.getMonths() == 3 && faboRTC.getDays()==isLastSundayOfTheMonthPassed() && faboRTC.getHours() == 2 && faboRTC.getMinutes() == 0 && dayLightSaving && dayLightSavingStat == 0)  //se durante il cambio dell'ora legale l'orologio è spento questa routine al primo avvio corregge l'orario
     {
       faboRTC.setHours(faboRTC.getHours() + 1);
@@ -91,9 +98,9 @@ void setup()
 void loop() {
 
      
-  buttonsCheck();
+  buttonsCheck(); //it continuosly check if the buttons are pressed
  
-  if(randHour == faboRTC.getHours() && randMin == faboRTC.getMinutes())    // randomizza il burnIn
+  if(randHour == faboRTC.getHours() && randMin == faboRTC.getMinutes())    //every time the BurnIn occcurs it set the next BurnIn time randomly
     {
       randHour = (randHour + random(8,12))%24;
       randomSeed(faboRTC.getMonths()+100*faboRTC.getHours()+faboRTC.getDays()*10000); 
@@ -103,5 +110,6 @@ void loop() {
   if(getButton3())
     settings();
   
-  writeTime(faboRTC.getHours(),faboRTC.getMinutes(),blinking && (millis()/1000)%2);
+  //refreshes the written time. 
+  writeTime(faboRTC.getHours(),faboRTC.getMinutes(),blinking && (millis()/1000)%2); 
 }
